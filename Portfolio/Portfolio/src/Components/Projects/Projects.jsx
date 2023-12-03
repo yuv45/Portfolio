0cbf7e256b8e670d2b235b88f53ef5631c1c871e
Sushant Arora CA <aroravinay2004@gmail.com>
2023-12-03 15:32:11 +0530

    My-portfolio

---
+++ Portfolio/Portfolio/src/Components/Projects/Projects.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div id='Projects'>
+        <div>
+            <h1>Projects</h1>
+            <div>
+              <div>
+              <img/>
+              </div>
+            <hr/>
+              <div>
+                   
+              </div>
+            
+              
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Projects
